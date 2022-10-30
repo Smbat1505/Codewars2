70c586758dd21a0e410234df848b9c1f99826284
Sem Antony <1Cor13a8@gmail.com>
2022-10-30 17:49:46 +0400

    add code

--- stringEndsWith.js
+++ stringEndsWith.js
@@ -2,3 +2,6 @@ function solution(str, ending) {
   // TODO: Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string). Examples: solution('abc', 'bc') // returns true solution('abc', 'd') // returns false
   return str.endsWith(ending);
 }
+
+solution("Sem", "e");
+console.log(solution("Sem", "m"));
